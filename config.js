62a3b4ae8dd312f884df13a69fe9b11d948a05a0
HTMLtester <160041881+HTMLtesterofficial@users.noreply.github.com>
2024-12-11 14:45:31 -0500

    register config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
             const result = document.getElementById("result");
 
             if (userId === placeholderId) {
-                result.innerHTML = '<span class="success">Congratulations, you have successfully registered your copy! To download your copy, please go<a href="https://example.com" target="_blank"> here</a><br> To learn how to install your copy, please refer<a href="https://example.com" target="_blank"> here</a> for a tutorial.';
+                result.innerHTML = '<span class="success">Congratulations, you have successfully registered your copy! To download your copy, please go<a href="https://drive.google.com/drive/folders/1kbkK9wUcnuQzd4oYMlVRyyFMEWJNkdIX?usp=sharing" target="_blank"> here</a><br> To learn how to install your copy, please refer<a href="https://example.com" target="_blank"> here</a> for a tutorial.';
             } else {
                 result.innerHTML = '<span class="error">Incorrect ID. Please retype your ID or try again later.</span>';
             }
