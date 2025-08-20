996f243b13b1eb043ec54647e9f908b257c5609d
Just Another Passionate Java Coder <160041881+JavaTesterOfficial@users.noreply.github.com>
2025-08-20 19:21:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
-        function checkId() {
-            const userId = document.getElementById("confirmationId").value;
-            const placeholderId = "86990628525028470457";
-            const result = document.getElementById("result");
+        // function checkId() {
+        //     const userId = document.getElementById("confirmationId").value;
+        //     const placeholderId = "86990628525028470457";
+        //     const result = document.getElementById("result");
 
-            if (userId === placeholderId) {
-                result.innerHTML = '<span class="success">Congratulations, you have successfully registered your copy! To download your copy, please go<a href="https://drive.google.com/drive/folders/1kbkK9wUcnuQzd4oYMlVRyyFMEWJNkdIX?usp=sharing" target="_blank"> here</a><br> To learn how to install your copy, please refer<a href="https://example.com" target="_blank"> here</a> for a tutorial.';
-            } else {
-                result.innerHTML = '<span class="error">Incorrect ID. Please retype your ID or try again later.</span>';
-            }
-        }
+        //     if (userId === placeholderId) {
+        //         result.innerHTML = '<span class="success">Congratulations, you have successfully registered your copy! To download your copy, please go<a href="https://drive.google.com/drive/folders/1kbkK9wUcnuQzd4oYMlVRyyFMEWJNkdIX?usp=sharing" target="_blank"> here</a><br> To learn how to install your copy, please refer<a href="https://example.com" target="_blank"> here</a> for a tutorial.';
+        //     } else {
+        //         result.innerHTML = '<span class="error">Incorrect ID. Please retype your ID or try again later.</span>';
+        //     }
+        // }
+
+//Note for viewer:
+//this file was originally used for the Bypass Launcher, which was a bypassing service I set up in 2025 for my school. It got discontinued, but you can go ahead and check it out on my repositories. The name is "Bypass Launcher"
